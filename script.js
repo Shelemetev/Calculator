b7f2a8bec4cf2b281c53b948ca8c0bcf957ecd1a
Egor <egor.shelemetev228@mail.ru>
2024-07-16 13:58:12 +1000

    Validation end

---
+++ script.js
@@ -0,0 +1,27 @@
+const inputExport = document.getElementById("export")
+const currency = document.querySelector(".currency")
+const numberInput = document.querySelectorAll('.form-control')
+
+numberInput.forEach(element => {
+    element.addEventListener('change', () => {
+        if (element.value < 0) {
+            
+            element.value = 0
+        }
+    })
+})
+
+inputExport.addEventListener("change", () => {
+    if (inputExport.value === "1") {
+        currency.innerHTML = "¥"
+    } else {
+        currency.innerHTML = "₩"
+    }
+})
+
+
+document.querySelector(".form").addEventListener("submit", (event) => {
+    event.preventDefault()
+    validation(this)
+})
+
