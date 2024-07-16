6bdee9ed74277151d75572a08b4e62b26ce847e9
Egor <egor.shelemetev228@mail.ru>
2024-07-16 14:38:50 +1000

    Result верстка

--- script.js
+++ script.js
@@ -4,21 +4,19 @@ const numberInput = document.querySelectorAll('.form-control')
 
 numberInput.forEach(element => {
     element.addEventListener('change', () => {
-        if (element.value < 0) {
-            
-            element.value = 0
+        if (element.value < 1) {
+            element.value = 1
         }
     })
 })
 
 inputExport.addEventListener("change", () => {
-    if (inputExport.value === "1") {
+    if (inputExport.value === "1" || inputExport.value === "2") {
         currency.innerHTML = "¥"
     } else {
         currency.innerHTML = "₩"
     }
 })
-
 
 document.querySelector(".form").addEventListener("submit", (event) => {
     event.preventDefault()
